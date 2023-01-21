540812ac1ac553324030752be83ee9d552ed0751
hey-now <rubinand22@gmail.com>
2023-01-21 13:36:39 -0800

    working Sidebar and Navbar components

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import {Ecommerce, Orders, Calendar, Employees, Stacked,
-Pyramid, Customers, Kanban, Area, Bar, Pie, Financial,
-ColorPicker, ColorMapping, Editor, Line  } from './pages';
+  Pyramid, Customers, Kanban, Area, Bar, Pie, Financial,
+  ColorPicker, ColorMapping, Editor, Line  } from './pages';
+  
+import { useStateContext } from './contexts/ContextProvider';
+  
 import './App.css'
 
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
   return (
     <div>
       <BrowserRouter>
