55aa9f7c3d283290ce48733ca63b7ed72c414715
hey-now <rubinand22@gmail.com>
2023-01-24 11:01:00 -0800

    adding Data and customization to Calendar

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -9,7 +9,11 @@ const Calendar = () => {
   return (
     <div className='m-2 md:m-10 mt-24 p-2 md:p-10, bg-white rounded-3xl'>
       <Header category='App' title='Calender' />
-      <ScheduleComponent>
+      <ScheduleComponent
+        height='650px'
+        eventSettings={{ dataSource: scheduleData }}
+        selectedDate={new Date(2021, 0, 10)}
+      >
         <Inject services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]} />
       </ScheduleComponent>
     </div>
