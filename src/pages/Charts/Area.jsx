b6e85e4ba26b3df83bc20791b58b665e78d9ca89
hey-now <rubinand22@gmail.com>
2023-01-24 15:34:44 -0800

    adding Line, Area, Bar charts and minor bug fixes for responisive design

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, DateTime, Legend, SplineAreaSeries,  } from '@syncfusion/ej2-react-charts';
+
+import { Header } from '../../components';
+import { areaCustomSeries, areaPrimaryXAxis, areaPrimaryYAxis } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
 
 const Area = () => {
+  const { currentMode } = useStateContext();
   return (
-    <div>Area</div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl dark:bg-main-dark-bg'>
+      <Header category='Area' title='Inflation Rate in Percentage' />
+      <ChartComponent
+        id='line-chart'
+        height='420px'
+        primaryXAxis={areaPrimaryXAxis}
+        primaryYAxis={areaPrimaryYAxis}
+        chartArea={{ border: {width: 0} }}
+        background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+      >
+        <Inject services={[SplineAreaSeries, DateTime, Legend]} />
+        <SeriesCollectionDirective>
+          {areaCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
   )
 }
 
-export default Area
+export default Area;
--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+
+import { Header, LineChart } from '../../components';
 
 const Line = () => {
   return (
-    <div>Line</div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl dark:bg-main-dark-bg'>
+      <Header category='Line' title='Inflation Rate' />
+      <div className='w-full'>
+        <LineChart />
+      </div>
+    </div>
   )
 }
 
